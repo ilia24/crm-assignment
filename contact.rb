5927ed672d655350a2219df09a3c345afc3d8ab5
ilia24 <iliademertchian24@gmail.com>
2017-04-02 15:56:01 -0400

    Add core readers, accessors, and class variables for Contact class

--- contact.rb
+++ contact.rb
@@ -1,4 +1,10 @@
 class Contact
+attr_reader :id
+attr_accessor :first_name, :last_name, :email, :note
+
+
+@@contacts = []
+@@id = 1
 
   # This method should initialize the contact's attributes
   def initialize(first_name, last_name, email, note)
