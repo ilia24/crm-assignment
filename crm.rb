841bdb118b7df8007d05f599772096bc8e759447
ilia24 <iliademertchian24@gmail.com>
2017-04-02 16:54:13 -0400

    Add initial menu functionality to CRM

--- crm.rb
+++ crm.rb
@@ -8,11 +8,21 @@ class CRM
   end
 
   def main_menu
-
+    while true
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
   end
 
   def call_option
